7940559a2705075bad9f1d1102b4161187f2bc78
Maykal Tenev <michaeltenev@gmail.com>
2022-08-21 21:02:24 +0200

    building API

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,5 @@
 // Synchronous => Blocking
-// const fs = require('fs');
+const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
@@ -29,17 +29,22 @@ const url = require('url');
 // console.log("Will read file!");
 /////
 //SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
 const server = http.createServer((req, res) => {
-    console.log(req.url);
     const pathName = req.url;
     if (pathName === '/' || pathName === '/overview') {
         res.end('Hello from the Overview!');
     } else if (pathName === '/product') {
         res.end('This is the PRODUCT')
+    } else if (pathName === '/api') {
+        res.writeHead(200, { 'Content-type': 'application/json' })
+        res.end(data)
     } else {
         res.writeHead(404, {
             'Content-Type': 'text/html',
-            'my-own-header': 'hello - world',
+            'my-own-header': 'hello-world',
         })
         res.end('<h1>Page not found!</h1>');
     }
