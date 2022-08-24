505942d8412af2d68ee7b7009ea5e87c646f23a5
Maykal Tenev <michaeltenev@gmail.com>
2022-08-24 11:25:15 +0200

    how node works - modules

--- 2-how-node-works/starter/modules.js
+++ 2-how-node-works/starter/modules.js
@@ -1,2 +1,20 @@
-console.log(arguments);
-console.log(require('module').wrapper)
+// console.log(arguments);
+// console.log(require('module').wrapper);
+
+// module.exports
+const C = require('./test-module-1');
+const calc1 = new C();
+console.log(calc1.add(2, 5))
+
+// exports
+// const calc2 = require('./test-module-2');
+// console.log(calc2.add(2, 5))
+//! We are able to destruct the object
+const { add, divide, multiply } = require('./test-module-2');
+console.log(add(2, 5))
+
+//caching
+//! The first console.log from the module logs only once and the other calls are stored and displayed from thea cash
+require('./test-module-3')();
+require('./test-module-3')();
+require('./test-module-3')();
---
+++ 2-how-node-works/starter/test-module-1.js
@@ -0,0 +1,14 @@
+module.exports = class {
+    // class Calculator {
+    add(a, b) {
+        return a + b;
+    }
+    multiply(a, b) {
+        return a * b;
+    }
+    divide(a, b) {
+        return a / b;
+    }
+}
+// Option 2
+// module.exports = Calculator;
