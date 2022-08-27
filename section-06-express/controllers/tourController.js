266145ecb31eb27006c7c06a64a13ae05394dc70
Maykal Tenev <michaeltenev@gmail.com>
2022-08-27 22:54:21 +0200

    fixed checkbody

--- section-06-express/controllers/tourController.js
+++ section-06-express/controllers/tourController.js
@@ -1,10 +1,34 @@
-const fs = require('fs')
+const fs = require('fs');
+
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+    console.log(`Tour id is: ${val}`);
+
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    next();
+};
+
+exports.checkBody = (req, res, next) => {
+    if (!req.body.name || !req.body.price) {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Missing name or price'
+        });
+    }
+    next();
+};
+
 exports.getAllTours = (req, res) => {
     console.log(req.requestTime);
+
     res.status(200).json({
         status: 'success',
         requestedAt: req.requestTime,
@@ -12,26 +36,26 @@ exports.getAllTours = (req, res) => {
         data: {
             tours
         }
-    })
+    });
 };
+
 exports.getTour = (req, res) => {
-    console.log(req.params)
+    console.log(req.params);
     const id = req.params.id * 1;
-    if (id > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        })
-    }
+
     const tour = tours.find(el => el.id === id);
+
     res.status(200).json({
         status: 'success',
         data: {
             tour
         }
-    })
-}
+    });
+};
+
 exports.createTour = (req, res) => {
+    // console.log(req.body);
+
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({ id: newId }, req.body);
 
@@ -46,33 +70,23 @@ exports.createTour = (req, res) => {
                 data: {
                     tour: newTour
                 }
-            })
-        })
-}
-exports.deleteTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
+            });
+        }
+    );
+};
+
+exports.updateTour = (req, res) => {
     res.status(200).json({
         status: 'success',
-        data: null
-    })
-}
-exports.updateTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
+        data: {
+            tour: '<Updated tour here...>'
+        }
+    });
+};
 
+exports.deleteTour = (req, res) => {
     res.status(204).json({
         status: 'success',
-        data: {
-            tour: '<Updated tour here....'
-        }
-    })
-}
+        data: null
+    });
+};
--- section-06-express/routes/tourRoutes.js
+++ section-06-express/routes/tourRoutes.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const tourController = require('./../controllers/tourController')
+const tourController = require('./../controllers/tourController');
+
 const router = express.Router();
 
-router.param('id', (req, res, next, val) => {
-    console.log(`Tour id is: ${val}`)
-    next();
-})
+router.param('id', tourController.checkID);
+
 router
     .route('/')
     .get(tourController.getAllTours)
-    .post(tourController.createTour);
+    .post(tourController.checkBody, tourController.createTour);
 
 router
     .route('/:id')
     .get(tourController.getTour)
     .patch(tourController.updateTour)
     .delete(tourController.deleteTour);
+
 module.exports = router;
