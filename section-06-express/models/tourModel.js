53a71c95e02c574030440a3fc45dc992be239877
Maykal Tenev <michaeltenev@gmail.com>
2022-08-29 23:12:25 +0200

    enriching the schema

--- section-06-express/models/tourModel.js
+++ section-06-express/models/tourModel.js
@@ -1,19 +1,58 @@
+/* eslint-disable prettier/prettier */
 const mongoose = require('mongoose');
 
 const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         require: [true, 'A tour must have name'],
-        unique: true
+        unique: true,
+        trim: true
     },
-    rating: {
+    duration: {
+        type: Number,
+        required: [true, 'A tour must have a duration']
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'A tour must have a group size']
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'A tour must have a difficulty']
+    },
+    ratingAverage: {
         type: Number,
         default: 4.5
+    },
+    ratingQuantity: {
+        type: Number,
+        default: 0
     },
     price: {
         type: Number,
         require: [true, 'A tour must have price']
     },
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true,
+        required: [true, 'A tour must have a description']
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'A tour must have a cover image']
+    },
+    images: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    startDates: [Date]
+
 })
 const Tour = mongoose.model('Tour', tourSchema);
 module.exports = Tour;
