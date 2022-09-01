1cd6e00a673829d6be02017af69cf23052fa18ee
Maykal Tenev <michaeltenev@gmail.com>
2022-09-01 16:09:47 +0200

    advanced filter

--- section-06-express/controllers/tourController.js
+++ section-06-express/controllers/tourController.js
@@ -32,7 +32,26 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
     try {
-        const tours = await Tour.find();
+        ///!Build Query
+        // 1) Filtering
+
+        const queryObj = { ...req.query };
+        const excludeFields = ['page', 'sort', 'limit', 'fields'];
+        excludeFields.forEach(el => delete queryObj[el]);
+
+        // 2) Advanced filtering
+
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+        console.log(JSON.parse(queryStr))
+        const query = await Tour.find(JSON.parse(queryStr));
+        //{ difficulty: 'easy', duration: { $gte: 5 } }
+
+
+
+        //! Execute Query
+        const tours = await query;
+        //! Send RESPONSE
         res.status(200).json({
             status: 'success',
             results: tours.length,
--- section-06-express/dev-data/data/import-dev-data.js
+++ section-06-express/dev-data/data/import-dev-data.js
@@ -4,19 +4,24 @@ const dotenv = require('dotenv');
 const Tour = require('./../../models/tourModel.js');
 
 dotenv.config({ path: './config.env' });
-const DB = process.env.DATABASE.replace('<PASSWORD>',
-    process.env.DATABASE_PASSWORD);
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
 
-mongoose.
-    connect(DB, {
+mongoose
+    .connect(DB, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,
         useUnifiedTopology: true,
-    }).then(() => console.log('DB connection successful!'))
+    })
+    .then(() => console.log('DB connection successful!'));
 
 // READ JSON FILE
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
 
 // IMPORT DATA INTO DB
 const importData = async () => {
@@ -26,7 +31,8 @@ const importData = async () => {
     } catch (err) {
         console.log(err);
     }
-}
+    process.exit();
+};
 // DELETE ALL DATA FROM DB
 const deleteData = async () => {
     try {
@@ -35,5 +41,11 @@ const deleteData = async () => {
     } catch (err) {
         console.log(err);
     }
+    process.exit();
+};
+if (process.argv[2] === '--import') {
+    importData();
+} else if (process.argv[2] === '--delete') {
+    deleteData();
 }
-console.log(process.argv)
+console.log(process.argv);
